f48bf80e3a99f8300d2d6a25b49fe70be619e698
Leanawyss <97448500+Leanawyss@users.noreply.github.com>
2022-01-10 13:52:18 +0100

    Update Prüfungssoftware.py

--- Prüfungssoftware.py
+++ Prüfungssoftware.py
@@ -1,12 +1,12 @@
 i=0
 frage=input("Wer ist der Präsident von der USA?")
-while i<4:
+while i<4: #Wiederholt das Programm bis zu drei mal.
     
-    if frage == "Joe Biden":
+    if frage == "Joe Biden": #Sorgt dafür, wenn die Frage richtig beantwortet wird, dass es aufhört.
         print ("Gratulation!")
         i=i+10
 #        break
-    else:
+    else: #Wenn die Frage nicht richtig beantwortet wird, wird die Frage noch einmal gestellt und es steht wie viele Versuche man noch hat.
         print ("Noch " + str(3-i) +" Versuche") 
         i=i+1
         frage=input("Wer ist der Präsident von der USA?")
